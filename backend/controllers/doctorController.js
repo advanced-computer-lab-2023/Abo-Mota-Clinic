26c26d2a69b5ce835f6f24055011da833b8490dd
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-10-05 20:02:06 +0300

    edit details of doctor

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,7 +1,20 @@
+const Doctor = require("../models/Doctor");
 
 // Edit Email, Affiliation, Rate
 const editDetails = async (req, res) => {
-    
+    try {
+        const { id } = req.params;
+        const filter = {_id:id};
+        const doctorExists = await Doctor.findOne(filter);
+        if (!doctorExists) {
+            throw new Error('This doctor does not exist');
+        }
+        const update = req.body;
+        const updatedDoctor = await Doctor.updateOne(filter, update);
+        res.status(200).json(updatedDoctor);
+    } catch(error) {
+        res.status(404).json({ error: error.message });
+    }
 }
 
 // View All Doctor's Appointments
