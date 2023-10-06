99b1c5b94bc945ef2e5c215623e4924d12b8e8d0
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2023-10-06 19:57:51 +0300

    adjusted admin controller functions and routes

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -5,43 +5,41 @@ const Prescription = require("../models/Prescription");
 
 // Get all patient prescriptions
 const getPrescriptions = async (req, res) => {
-	try {
-		const prescriptions = await Prescription.find({ patient: req.body.patientId });
-		res.status(200).json(prescriptions);
-	} catch (error) {
-		res.status(500).json({ error: "Failed to retrieve prescriptions" });
-	}
+	// try {
+	// 	const prescriptions = await Prescription.find({ patient: req.body.patientId });
+	// 	res.status(200).json(prescriptions);
+	// } catch (error) {
+	// 	res.status(500).json({ error: "Failed to retrieve prescriptions" });
+	// }
 };
 
 // Get all patient's registered family members
 const getFamilyMembers = async (req, res) => {
-	try {
-		const patient = await Patient.findById(req.body.patientId).populate("familyMembers");
-		res.status(200).json(patient.familyMembers);
-	} catch (error) {
-		res.status(500).json({ error: "Failed to retrieve family members" });
-	}
+	// try {
+	// 	const patient = await Patient.findById(req.body.patientId).populate("familyMembers");
+	// 	res.status(200).json(patient.familyMembers);
+	// } catch (error) {
+	// 	res.status(500).json({ error: "Failed to retrieve family members" });
+	// }
 };
 
 // Add a patient's family member
 const addFamilyMember = async (req, res) => {
-	try {
-		const { nationalId } = req.body;
-		const familyMember = await Patient.find({ nationalId: nationalId });
-
-		patient.familyMembers.push(familyMember._id);
-		await patient.save();
-
-		res.status(200).json({ message: "Family member added successfully" });
-	} catch (error) {
-		res.status(500).json({ error: "Failed to add family member" });
-	}
+	// try {
+	// 	const { nationalId } = req.body;
+	// 	const familyMember = await Patient.find({ nationalId: nationalId });
+	// 	patient.familyMembers.push(familyMember._id);
+	// 	await patient.save();
+	// 	res.status(200).json({ message: "Family member added successfully" });
+	// } catch (error) {
+	// 	res.status(500).json({ error: "Failed to add family member" });
+	// }
 };
 
 // Get all doctors
 const getDoctors = async (req, res) => {
 	try {
-		const doctors = await Doctor.find().select("-password"); // Excluding password field from the output
+		const doctors = await Doctor.find({ registrationStatus: "approved" }).select("-password"); // Excluding password field from the output
 		res.status(200).json(doctors);
 	} catch (error) {
 		res.status(500).json({ error: "Failed to retrieve doctors" });
@@ -50,13 +48,12 @@ const getDoctors = async (req, res) => {
 
 // Get all appointments
 const getAppointments = async (req, res) => {
-	try {
-		const appointments = await Appointment.find({ patient: req.params.patientId });
-
-		res.status(200).json(appointments);
-	} catch (error) {
-		res.status(500).json({ error: "Failed to retrieve appointments" });
-	}
+	// try {
+	// 	const appointments = await Appointment.find({ patient: req.params.patientId });
+	// 	res.status(200).json(appointments);
+	// } catch (error) {
+	// 	res.status(500).json({ error: "Failed to retrieve appointments" });
+	// }
 };
 
 module.exports = {
