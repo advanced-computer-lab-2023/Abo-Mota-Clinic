5aa618bfb590417ae66a16fd9de1a1deab2b63d1
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2023-11-08 21:54:21 +0200

    added wallet to doctor and patient model

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -2,60 +2,64 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const doctorSchema = new Schema(
-  {
-    name: String,
-    username: String,
-    password: String,
-    email: String,
-    dob: Date,
-    rate: Number,
-    affiliation: String,
-    speciality: String,
-    educationalBackground: String,
-    nationalId: String,
-    medicalLicense: {
-      data: Buffer,
-      contentType: String,
-    },
-    medicalDegree: {
-      data: Buffer,
-      contentType: String,
-    },
-    registrationStatus: {
-      type: String,
-      enum: ["pending", "approved", "rejected"],
-      default: "pending",
-    },
-    // patients: [
-    // 	{
-    // 		type: Schema.Types.ObjectId,
-    // 		ref: "Patient",
-    // 	},
-    // ],
-    // prescriptions: [
-    // 	{
-    // 		type: Schema.Types.ObjectId,
-    // 		ref: "Prescription",
-    // 	},
-    // ],
-    // appointments: [
-    // 	{
-    // 		type: Schema.Types.ObjectId,
-    // 		ref: "Appointment",
-    // 	},
-    // ],
-  },
-  { toJSON: { virtuals: true } }
+	{
+		name: String,
+		username: String,
+		password: String,
+		email: String,
+		dob: Date,
+		rate: Number,
+		affiliation: String,
+		speciality: String,
+		educationalBackground: String,
+		nationalId: String,
+		medicalLicense: {
+			data: Buffer,
+			contentType: String,
+		},
+		medicalDegree: {
+			data: Buffer,
+			contentType: String,
+		},
+		registrationStatus: {
+			type: String,
+			enum: ["pending", "approved", "rejected"],
+			default: "pending",
+		},
+		wallet: {
+			type: Number,
+			default: 0,
+		},
+		// patients: [
+		// 	{
+		// 		type: Schema.Types.ObjectId,
+		// 		ref: "Patient",
+		// 	},
+		// ],
+		// prescriptions: [
+		// 	{
+		// 		type: Schema.Types.ObjectId,
+		// 		ref: "Prescription",
+		// 	},
+		// ],
+		// appointments: [
+		// 	{
+		// 		type: Schema.Types.ObjectId,
+		// 		ref: "Appointment",
+		// 	},
+		// ],
+	},
+	{ toJSON: { virtuals: true } }
 );
 
 const options = {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
+	year: "numeric",
+	month: "2-digit",
+	day: "2-digit",
 };
 
 doctorSchema.virtual("formattedDob").get(function () {
-  return new Intl.DateTimeFormat("en-US", options).format(this.dob);
+	return new Intl.DateTimeFormat("en-US", options).format(this.dob);
 });
 
 const Doctor = mongoose.model("Doctor", doctorSchema);
