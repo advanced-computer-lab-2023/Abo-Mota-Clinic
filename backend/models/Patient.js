63396798799a58f9f8b2fb295190d93e6c0a7ea1
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-11-12 02:43:56 +0200

    fixing register doctor and viewing uploads

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -4,102 +4,102 @@ const Joi = require("joi");
 const { Schema } = mongoose;
 // ADDDDD NATIONALID PICTURE ATTRIBUTE YALLA
 const patientSchema = new Schema(
-	{
-		name: String,
-		username: String,
-		password: String,
-		email: String,
-		dob: Date,
-		gender: String,
-		mobile: String,
-		nationalId: String,
-		wallet: {
-			type: Number,
-			default: 0,
-		},
-		familyDiscount: {
-			type: Number,
-			default: 0,
-		},
-		familyMembers: [
-			{
-				name: String,
-				age: Number,
-				gender: String,
-				relationToPatient: {
-					type: String,
-					enum: ["child", "husband", "wife"],
-				},
-				nationalId: String,
-			},
-		],
-		linkedFamily: [
-			{
-				member: {
-					type: Schema.Types.ObjectId,
-					ref: "ClinicPatient",
-				},
-				relationToPatient: {
-					type: String,
-				},
-			},
-		],
-		emergencyContact: {
-			name: String,
-			mobile: String,
-			relation: String,
-		},
-		healthPackage: {
-			status: {
-				type: String,
-				default: null,
-				enum: ["subscribed", "unsubscribed", "cancelled", null],
-			},
-			package: {
-				type: Schema.Types.ObjectId,
-				default: null,
-				ref: "HealthPackage",
-			},
-			endDate: Date,
-			cancelDate: Date,
-			unsubscribeDate: Date,
-		},
-		healthRecords: [
-			{
-				data: Buffer,
-				mimetype: String,
-			},
-		],
-		medicalHistory: [
-			{
-				data: Buffer,
-				mimetype: String,
-			},
-		],
-		// prescriptions: [
-		//   {
-		//     type: Schema.Types.ObjectId,
-		//     ref: "Prescription",
-		//   },
-		// ],
-		// appointments: [
-		//   {
-		//     type: Schema.Types.ObjectId,
-		//     ref: "Appointment",
-		//   },
-		// ],
-	},
-	{ toJSON: { virtuals: true } }
+  {
+    name: String,
+    username: String,
+    password: String,
+    email: String,
+    dob: Date,
+    gender: String,
+    mobile: String,
+    nationalId: String,
+    wallet: {
+      type: Number,
+      default: 0,
+    },
+    familyDiscount: {
+      type: Number,
+      default: 0,
+    },
+    familyMembers: [
+      {
+        name: String,
+        age: Number,
+        gender: String,
+        relationToPatient: {
+          type: String,
+          enum: ["child", "husband", "wife"],
+        },
+        nationalId: String,
+      },
+    ],
+    linkedFamily: [
+      {
+        member: {
+          type: Schema.Types.ObjectId,
+          ref: "ClinicPatient",
+        },
+        relationToPatient: {
+          type: String,
+        },
+      },
+    ],
+    emergencyContact: {
+      name: String,
+      mobile: String,
+      relation: String,
+    },
+    healthPackage: {
+      status: {
+        type: String,
+        default: null,
+        enum: ["subscribed", "unsubscribed", "cancelled", null],
+      },
+      package: {
+        type: Schema.Types.ObjectId,
+        default: null,
+        ref: "HealthPackage",
+      },
+      endDate: Date,
+      cancelDate: Date,
+      unsubscribeDate: Date,
+    },
+    healthRecords: [
+      {
+        data: Buffer,
+        contentType: String,
+      },
+    ],
+    medicalHistory: [
+      {
+        data: Buffer,
+        contentType: String,
+      },
+    ],
+    // prescriptions: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Prescription",
+    //   },
+    // ],
+    // appointments: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Appointment",
+    //   },
+    // ],
+  },
+  { toJSON: { virtuals: true } }
 );
 
 const options = {
-	year: "numeric",
-	month: "2-digit",
-	day: "2-digit",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
 };
 
 patientSchema.virtual("formattedDob").get(function () {
-	return new Intl.DateTimeFormat("en-US", options).format(this.dob);
+  return new Intl.DateTimeFormat("en-US", options).format(this.dob);
 });
 
 const Patient = mongoose.model("ClinicPatient", patientSchema);
