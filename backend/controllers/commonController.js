72a59c96969e8114df39431469e1791159928421
Aly Raafat <67808306+alyraafat@users.noreply.github.com>
2023-12-17 04:26:41 +0200

    fixed backend bug

--- backend/controllers/commonController.js
+++ backend/controllers/commonController.js
@@ -1,6 +1,6 @@
 const Patient = require("../models/Patient");
 const Doctor = require("../models/Doctor");
-const Admin = require("../models/Admin");
+const Admin = require("../models/ClinicAdmin");
 const Message = require("../models/Message");
 const Notification = require("../models/Notification");
 const sendEmail = require("../utils/sendEmail");
@@ -13,16 +13,14 @@ const getMessages = async (req, res) => {
 
     let sender;
 
-    if (userType.toLowerCase() === 'doctor')
-      sender = await Doctor.findOne({ username });
-    if (userType.toLowerCase() === 'patient')
-      sender = await Patient.findOne({ username });
+    if (userType.toLowerCase() === "doctor") sender = await Doctor.findOne({ username });
+    if (userType.toLowerCase() === "patient") sender = await Patient.findOne({ username });
 
     const messages = await Message.find({
       $or: [
         { sender: sender._id, recipient: recipient },
-        { sender: recipient, recipient: sender._id }
-      ]
+        { sender: recipient, recipient: sender._id },
+      ],
     });
     res.status(200).json({ messages });
   } catch (error) {
@@ -38,21 +36,18 @@ const sendMessage = async (req, res) => {
 
     let sender;
 
-    if (userType.toLowerCase() === 'doctor')
-      sender = await Doctor.findOne({ username });
-    if (userType.toLowerCase() === 'patient')
-      sender = await Patient.findOne({ username });
+    if (userType.toLowerCase() === "doctor") sender = await Doctor.findOne({ username });
+    if (userType.toLowerCase() === "patient") sender = await Patient.findOne({ username });
 
     const message = {
       content,
       sender: sender._id,
       recipient: recipient,
       date,
-    }
+    };
 
     await Message.create(message);
     res.status(200).json({ message: "Message sent successfully!" });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -63,17 +58,16 @@ const getNotifications = async (req, res) => {
     const { username, userType } = req.userData;
 
     let user;
-    if (userType.toLowerCase() === 'patient')
-      user = await Patient.findOne({ username });
-    if (userType.toLowerCase() === 'doctor')
-      user = await Doctor.findOne({ username });
+    if (userType.toLowerCase() === "patient") user = await Patient.findOne({ username });
+    if (userType.toLowerCase() === "doctor") user = await Doctor.findOne({ username });
 
-    const notifications = await Promise.all(user.notifications.map(async (notificationId) => {
-      return await Notification.findOne({ _id: notificationId });
-    }));
+    const notifications = await Promise.all(
+      user.notifications.map(async (notificationId) => {
+        return await Notification.findOne({ _id: notificationId });
+      })
+    );
 
     res.status(200).json({ notifications: notifications });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -92,11 +86,11 @@ const sendEmailNotif = async (req, res) => {
     };
 
     await sendEmail(mailOptions);
-    res.status(200).send('Email sent successfully');
+    res.status(200).send("Email sent successfully");
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const sendNotification = async (req, res) => {
   try {
@@ -104,13 +98,12 @@ const sendNotification = async (req, res) => {
     const { recipientUsername, recipientType, content } = req.body;
 
     let recipient;
-    if (recipientType.toLowerCase() === 'patient')
+    if (recipientType.toLowerCase() === "patient")
       recipient = await Patient.findOne({ username: recipientUsername.toLowerCase() });
-    if (recipientType.toLowerCase() === 'doctor')
+    if (recipientType.toLowerCase() === "doctor")
       recipient = await Doctor.findOne({ username: recipientUsername.toLowerCase() });
 
-    if (!recipient)
-      throw new Error("This recipient does not exist");
+    if (!recipient) throw new Error("This recipient does not exist");
 
     const notification = {
       sender: {
@@ -123,7 +116,7 @@ const sendNotification = async (req, res) => {
       },
       content: content,
       date: Date.now(),
-    }
+    };
 
     const savedNotification = await Notification.create(notification);
 
@@ -132,11 +125,9 @@ const sendNotification = async (req, res) => {
     await recipient.save();
 
     res.status(200).json({ message: "Notification sent successfully!" });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-
 };
 
 const getLoggedIn = async (req, res) => {
@@ -145,14 +136,11 @@ const getLoggedIn = async (req, res) => {
 
     let user;
 
-    if (userType.toLowerCase() === 'patient')
-      user = await Patient.findOne({ username });
+    if (userType.toLowerCase() === "patient") user = await Patient.findOne({ username });
 
-    if (userType.toLowerCase() === 'doctor')
-      user = await Doctor.findOne({ username });
+    if (userType.toLowerCase() === "doctor") user = await Doctor.findOne({ username });
 
-    if (userType.toLowerCase() === 'admin')
-      user = await Admin.findOne({ username });
+    if (userType.toLowerCase() === "admin") user = await Admin.findOne({ username });
 
     res.status(200).json(user);
   } catch (error) {
@@ -166,10 +154,8 @@ const getRecipient = async (req, res) => {
     const { recipientId } = req.query;
 
     let user;
-    if (userType.toLowerCase() === 'patient')
-      user = await Doctor.findOne({ _id: recipientId });
-    if (userType.toLowerCase() === 'doctor')
-      user = await Patient.findOne({ _id: recipientId });
+    if (userType.toLowerCase() === "patient") user = await Doctor.findOne({ _id: recipientId });
+    if (userType.toLowerCase() === "doctor") user = await Patient.findOne({ _id: recipientId });
 
     res.status(200).json(user);
   } catch (error) {
@@ -178,20 +164,23 @@ const getRecipient = async (req, res) => {
 };
 
 const getContactDetails = async (loggedIn, contactIds, collection) => {
-  const details = await Promise.all(contactIds.map(async (contactId) => {
+  const details = await Promise.all(
+    contactIds.map(async (contactId) => {
+      const latestMessage = await Message.findOne({
+        $or: [
+          { sender: loggedIn._id, recipient: contactId },
+          { sender: contactId, recipient: loggedIn._id },
+        ],
+      })
+        .sort({ date: -1 })
+        .lean();
 
-    const latestMessage = await Message.findOne({
-      $or: [
-        { sender: loggedIn._id, recipient: contactId },
-        { sender: contactId, recipient: loggedIn._id }
-      ]
-    }).sort({ date: -1 }).lean();
-
-    return {
-      contact: await collection.findById(contactId).lean(),
-      message: latestMessage,
-    }
-  }));
+      return {
+        contact: await collection.findById(contactId).lean(),
+        message: latestMessage,
+      };
+    })
+  );
 
   return details.sort((a, b) => {
     const dateA = new Date(a.message.date);
@@ -200,25 +189,23 @@ const getContactDetails = async (loggedIn, contactIds, collection) => {
     // Sort in descending order (newest to oldest)
     return dateB - dateA;
   });
-}
-
+};
 
 const getContactedUsers = async (req, res) => {
   try {
-
     const username = req.userData.username;
     const userType = req.userData.userType;
 
-    const sameCollection = userType.toLowerCase() === 'patient' ? Patient : Doctor;
-    const oppositeCollection = userType.toLowerCase() === 'patient' ? Doctor : Patient;
+    const sameCollection = userType.toLowerCase() === "patient" ? Patient : Doctor;
+    const oppositeCollection = userType.toLowerCase() === "patient" ? Doctor : Patient;
 
     const loggedIn = await sameCollection.findOne({ username });
     const sentMessages = await Message.find({ sender: loggedIn._id });
     const receivedMessages = await Message.find({ recipient: loggedIn._id });
 
     // duplicate-free contacted users
-    const recipientIds = [...new Set(sentMessages.map(message => message.recipient.toString()))];
-    const senderIds = [...new Set(receivedMessages.map(message => message.sender.toString()))];
+    const recipientIds = [...new Set(sentMessages.map((message) => message.recipient.toString()))];
+    const senderIds = [...new Set(receivedMessages.map((message) => message.sender.toString()))];
     const contactedUserIds = [...new Set([...recipientIds, ...senderIds])];
     const contactedUsers = await getContactDetails(loggedIn, contactedUserIds, oppositeCollection);
 
@@ -226,28 +213,26 @@ const getContactedUsers = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const nil = async (req, res) => {
   res.status(200).json("You just wasted everyone's itme");
-}
+};
 
-const getUser = async(req,res) => {
-  try{
-    const {id} = req.query;
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.query;
     let user = await Doctor.findOne({ _id: id });
-    if(!user){
+    if (!user) {
       user = await Patient.findOne({ _id: id });
     }
-    if(!user)
-      return res.status(400).json({message: "User not found"});
+    if (!user) return res.status(400).json({ message: "User not found" });
 
-    return res.status(200).json({name: user.name, username: user.username});
-
-  }catch(error){
+    return res.status(200).json({ name: user.name, username: user.username });
+  } catch (error) {
     return res.status(500).json({ error: error.message });
   }
-}
+};
 
 module.exports = {
   sendMessage,
